243c09ebdecfefb54d1417b323a6c7ad14b96bc6
reeneeramirez9@gmail.com <>
2014-08-13 18:01:11 +0000

    main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Initialize Phaser Engine. Create a 400x490px game!
+
+var game = new Phaser.Game(400,490, Phasae.AUTO, 'gameDiv');
+
+//Creat our 'main' state that will contain the game
+//This is the body of the game itself
+//It should contain all code related to the game itself
+
+var mainState = {
+  preload: function () {
+  //This fundtion wii execute at the beginning
+  //Which is where we'll load out assets for the game
+  
+  //Set the background color of the game
+  game.stage.backgroundColor = "#71c5cf";
+    
+},
+  create: function () {
+  
+  //This function is called right after preload function
+  //This is where we set up the game assets from earlier
+},
+  update: function () {
+  //This function runs 60 times per second
+},
+
+}
+
+//Add and start the 'mainState' to start the game
+game.state.add('main', mainState);
+gaem.state.start('main');
