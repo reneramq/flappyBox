070502f89933b384e4cd15307706da5651ce442d
reeneeramirez9@gmail.com <>
2014-08-14 15:38:53 +0000

    flappybox

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Initialize Phaser Engine. Create a 400x490px game!
 
-var game = new Phaser.Game(400,490, Phasae.AUTO, 'gameDiv');
+var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
 
 //Creat our 'main' state that will contain the game
 //This is the body of the game itself
@@ -8,24 +8,38 @@ var game = new Phaser.Game(400,490, Phasae.AUTO, 'gameDiv');
 
 var mainState = {
   preload: function () {
-  //This fundtion wii execute at the beginning
+  //This function will execute at the beginning
   //Which is where we'll load out assets for the game
   
   //Set the background color of the game
   game.stage.backgroundColor = "#71c5cf";
+  game.load.image('bird', 'assets/bird.png');
+  game.load.image('pipe', 'assets/pipe.png');
     
 },
   create: function () {
   
   //This function is called right after preload function
   //This is where we set up the game assets from earlier
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  this.bird = this.game.add.sprite(100, 245, 'bird');
+  
+  //Now that we have a bird and gravity...we need to tell the bird
+  //to react to the gravity
+  
+  game.physics.arcade.enable(this.bird);
+  
+  this.bird.body.gravity.y = 1000;
+
 },
   update: function () {
   //This function runs 60 times per second
 },
 
-}
+
+};
 
 //Add and start the 'mainState' to start the game
 game.state.add('main', mainState);
-gaem.state.start('main');
+game.state.start('main');
