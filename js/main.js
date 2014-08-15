e501f73a82e68904d86d2becb57874700060f554
reeneeramirez9@gmail.com <>
2014-08-15 15:41:48 +0000

    Pipes work

--- js/main.js
+++ js/main.js
@@ -39,6 +39,9 @@ var mainState = {
   this.pipes.enableBody = true;
   this.pipes.createMultiple(20, 'pipe');
   this.time = game.time.events.loop(1500, this.addRowOfPipes, this);
+  this.score = 0;
+  this.labelScore = game.add.text(20,20, "0", {font: "30px Arial", fill: "#ffffff"});
+  
    
 },
   update: function () {
@@ -47,6 +50,8 @@ var mainState = {
   if (this.bird.inWorld == false) {
     this.restartGame();
   }
+  
+  game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 },
 
 addOnePipe: function (x,y) {
@@ -68,6 +73,8 @@ addRowOfPipes: function () {
     if (i != hole && i != hole + 1){
       this.addOnePipe(400, i*60 + 10);
     }
+    this.score += 1;
+    this.labelScore.text = this.score;
 },
 
   jump: function () {
